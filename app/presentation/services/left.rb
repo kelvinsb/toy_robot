309c779b933add47c215f3a63b3bc58e987e8019
kelvinsb <kelvinbr11@gmail.com>
2022-07-26 21:38:29 -0300

    fix: typo

--- app/presentation/services/left.rb
+++ app/presentation/services/left.rb
@@ -4,10 +4,10 @@ require './app/presentation/utils'
 def left_service(id, command, games)
   raise ArgumentError, 'id required' unless id
 
-  game_finded = games.find_game(id)
-  return {} if game_finded.nil?
+  game_found = games.find_game(id)
+  return {} if game_found.nil?
 
-  result_command = left_command(command, game_finded)
+  result_command = left_command(command, game_found)
   return {} if result_command.nil?
 
   format_output(result_command)
--- app/presentation/services/report.rb
+++ app/presentation/services/report.rb
@@ -4,10 +4,10 @@ require './app/presentation/utils'
 def report_service(id, command, games)
   raise ArgumentError, 'id required' unless id
 
-  game_finded = games.find_game(id)
-  return {} if game_finded.nil?
+  game_found = games.find_game(id)
+  return {} if game_found.nil?
 
-  result_command = report_command(command, game_finded)
+  result_command = report_command(command, game_found)
   return {} if result_command.empty?
 
   position_x, position_y, facing = result_command.split(',')
